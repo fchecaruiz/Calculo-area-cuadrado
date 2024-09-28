00deeb7c87c6b0ed6abe9652909284eac619b570
fchecaruiz <130483176+fchecaruiz@users.noreply.github.com>
2024-09-28 22:27:02 +0200

    Add files via upload

---
+++ Calculo_area_cuadrado.py
@@ -0,0 +1,20 @@
+longitud = int(input("Introduce la longitud del lado de un cuadrado: "))
+longitud2 = int(input("Introduce otra longitud del lado de un cuadrado: "))
+
+area = longitud * longitud2
+perimetro = longitud * 4
+print(f"El área del cuadrado es {area} y el perimetro es {perimetro}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
